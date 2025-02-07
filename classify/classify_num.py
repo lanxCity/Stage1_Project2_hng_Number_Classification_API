b58e22a7f0e481b0c6941e768ab41fd18d5caa07
Abdulramon Sodiq <sodiqramon0@gmail.com>
2025-02-08 00:59:51 +0100

    Correction 8

--- classify/classify_num.py
+++ classify/classify_num.py
@@ -34,10 +34,10 @@ def classify_func(num):
         """
         # if num < 0:  # Perfect numbers must be positive
         #     return False
-
-        str_num = str(abs(original_num))
+        num = abs(original_num)
+        str_num = str(num)
         power = len(str_num)
-        return sum(int(i) ** power for i in str_num) == original_num
+        return sum(int(i) ** power for i in str_num) == num
 
     def is_perfect():
         """
@@ -85,3 +85,6 @@ def classify_func(num):
         "properties": properties,
         "digit_sum": digit_sum(),
     }
+
+
+print(classify_func(-5))
